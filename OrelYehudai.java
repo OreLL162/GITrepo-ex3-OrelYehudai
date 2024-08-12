e84b2cece6ecec6cdfa868b2f7f229ab6d1c934d
Orel Yehudai <103596170+OreLL162@users.noreply.github.com>
2024-08-12 15:40:18 +0300

    After 2nd change

--- OrelYehudai.java
+++ OrelYehudai.java
@@ -1,7 +1,7 @@
 public class OrelYehudai {
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-        System.out.println("Orel Yehudai");
+        System.out.println("Orel Yehudai after changed the text");
         }
         }
 }
